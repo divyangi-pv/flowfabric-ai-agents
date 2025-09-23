76642f68440ea406ba95cbe379299905c7c5bda1
Divyangi Mayank <137161633+divyangi-pv@users.noreply.github.com>
2025-09-23 12:42:04 +0530

    Update tests_triaging_assistant.py

--- mcp_tools/tests_triaging_assistant.py
+++ mcp_tools/tests_triaging_assistant.py
@@ -1,7 +1,20 @@
 from dotenv import load_dotenv
 from fastmcp import FastMCP
-from pydantic import BaseModel
 
 load_dotenv()
 
-mcp = FastMCP("tests-triaging-assistant")
+mcp = FastMCP("deployment-assistant")
+
+# -----------------------------
+# Schemas
+# -----------------------------
+
+# -----------------------------
+# MCP Tool Implementation
+# -----------------------------
+
+# -----------------------------
+# Entry Point
+# -----------------------------
+if __name__ == "__main__":
+    mcp.run()
